6721cce0bcae5235798af93aed6d76ea87e44ce2
Rajat Sharma <rajatsharma221098@gmail.com>
2025-03-10 20:32:40 +0530

    code optimize

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,17 @@
 import React from 'react';
 import './App.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import { BrowserRouter } from 'react-router-dom';
-// import { ThemeProvider } from 'styled-components';
-// import useDarkMode from 'use-dark-mode';
-// import AppContext from './AppContext';
 import MainApp from './MainApp';
-// import GlobalStyles from './theme/GlobalStyles';
-// import { lightTheme, darkTheme } from './theme/themes';
 
 function App() {
   window.matchMedia = null;
-  // const darkMode = useDarkMode(true);
 
   return (
-    // <AppContext.Provider value={{ darkMode }}>
-    //   <ThemeProvider theme={darkMode.value ? darkTheme : lightTheme}>
-    //     <GlobalStyles />
-    //     <div className="App">
-    //       <BrowserRouter>
             <MainApp />
-    //       </BrowserRouter>
-    //     </div>
-    //   </ThemeProvider>
-    // </AppContext.Provider>
   );
 }
 
 export default App;
+
+
+// npm run deploy - to deploy the app
+// npm run build -  to build the app
