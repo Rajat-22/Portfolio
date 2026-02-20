edade2113464e936aa07dfcc0c47528298998e83
RAJAT SHARMA <56541315+Rajat-22@users.noreply.github.com>
2026-02-20 12:03:54 +0530

    Update educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -3,18 +3,21 @@ export const educations = [
     id: 1,
     title: "Bachelor Degree",
     duration: "2017 - 2021",
-    institution: "Dr. A. P. J. Abdul Kalam Technical University, Lucknow, Uttar Pradesh, India",
+    institution: "Dr. A.P.J. Abdul Kalam Technical University, U.P., India",
+    percentage: "8.06 CGPA ≈ 73.1 %",
   },
   {
     id: 2,
     title: "Higher Secondary Certificate",
     duration: "2014 - 2016",
     institution: "Subhash Academy (CBSE)",
+    percentage: "76.6 %",
   },
   {
     id: 3,
     title: "Secondary School Certificate",
     duration: "2012 - 2014",
     institution: "Subhash Academy (CBSE)",
+    percentage: "8.8 CGPA ≈ 83.6 %",
   }
 ]
